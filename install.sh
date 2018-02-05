d2023649d3100f1ed7c85ecdd6fbac3a63476a96
Theodore R. Smith <theodore@phpexperts.pro>
2018-02-05 03:46:41 -0600

    Added a colorified ABOUT box to the installer.

--- install.sh
+++ install.sh
@@ -10,8 +10,23 @@
 ################################################################
 
 clear
-printf "Welcome to the Laravel QuickStart installation!\n"
-printf "    A PHP Experts, Inc., Project.\n\n"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+printf "${GREEN}"
+echo "################################################################"
+echo '#   The Laravel QuickStart Project                             #'
+echo '#   https://github.com/phpexpertsinc/laravel_quickstart        #'
+echo '#   License: MIT                                               #'
+echo '#                                                              #'
+echo '#   Copyright © 2018 PHP Experts, Inc. <sales@phpexperts.pro>  #'
+echo '#       Author: Theodore R. Smith <theodore@phpexperts.pro>    #'
+echo '#      PGP Sig: 4BF826131C3487ACD28F2AD8EB24A91DD6125690       #'
+echo '################################################################'
+printf "${NC}\n"
+
 
 if [ "$1" == "--help" ] || [ "$1" == "help" ];
 then
